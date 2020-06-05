f3a6d359b189ec62fd978246850f2077cc2db8ab
maoch1 <maoch1@midea.com>
2020-06-05 09:35:27 +0800

    sh增加注释说明

--- startup.sh
+++ startup.sh
@@ -8,6 +8,11 @@ project_jar="${project_name}.jar"
 project_log="${project_name}.log"
 
 #这个命令也可以kill掉
+#grep -v grep :grep -v意思是不包括，查找出了grep的所有信息
+#awk '{print $2}' 取出第二个字段
+#xargs将标准输入转化为命令行参数：echo '4567'|xargs kill -9 等同于：echo '4567' ;kill -9 4567
+#xargs 是给命令传递参数的一个过滤器，也是组合多个命令的一个工具。一般是和管道一起使用。
+#xargs 是一个强有力的命令，它能够捕获一个命令的输出，然后传递给另外一个命令。之所以能用到这个命令，关键是由于很多命令不支持|管道来传递参数，而日常工作中有有这个必要，所以就有了 xargs 命令
 #ps -ef|grep $project_jar|grep -v grep|awk '{print $2}'|xargs kill -9
 
 usage() {
